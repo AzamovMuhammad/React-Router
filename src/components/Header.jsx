6a73034ae45a225dfee8f51710b36f2d2f5bf13b
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-08 00:12:11 +0500

    navbar finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+
+import logo from '../assets/imgs/logo.png'
+import profile from '../assets/imgs/unnamed.png'
+
 import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdSearch } from "react-icons/io";
+import { GiSettingsKnobs } from "react-icons/gi";
+import { GrCircleQuestion } from "react-icons/gr";
+import { MdOutlineSettings } from "react-icons/md";
+import { TbGridDots } from "react-icons/tb";
+
 
 function Header() {
   return (
     <div className='navbar'>
         <div className='navLogo'>
-            <GiHamburgerMenu/>
+            <i><GiHamburgerMenu/></i>
+            <img src={logo} alt="" />
+        </div>
+        <div className="navSearch">
+            <i><IoMdSearch/></i>
+            <input type="text"  placeholder='Search mail'/>
+            <i><GiSettingsKnobs/></i>
+        </div>
+        <div className="navSettings">
+            <i><GrCircleQuestion/></i>
+            <i><MdOutlineSettings/></i>
+            <i><TbGridDots/></i>
+            <img src={profile} alt="" />
         </div>
     </div>
   )
