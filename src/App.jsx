f2d49a82a5a6fd4caa030ac08eab17ea37329ff0
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-07 22:44:28 +0500

    connected all pages with route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Inbox from './pages/Inbox'
+import Starred from './pages/Starred'
+import Snoozed from './pages/Snoozed'
+import Sent from './pages/Sent'
+import Drafts from './pages/Drafts'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
-          <Route></Route>
+          <Route path='/' element={<Inbox/>}></Route>
+          <Route path='/starred' element={<Starred/>}></Route>
+          <Route path='/snoozed' element={<Snoozed/>}></Route>
+          <Route path='/sent' element={<Sent/>}></Route>
+          <Route path='/drafts' element={<Drafts/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
