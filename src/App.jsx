9396683547dab1c94607b2690d4eae67ee96ecc9
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-07 22:20:56 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
