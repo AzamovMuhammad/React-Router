385556784c36b822e50ecbd349e15e898b36a518
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-07 22:45:40 +0500

    created not found page and connected with router

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Starred from './pages/Starred'
 import Snoozed from './pages/Snoozed'
 import Sent from './pages/Sent'
 import Drafts from './pages/Drafts'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/snoozed' element={<Snoozed/>}></Route>
           <Route path='/sent' element={<Sent/>}></Route>
           <Route path='/drafts' element={<Drafts/>}></Route>
+          <Route path='*' element={<NotFoundPage/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
