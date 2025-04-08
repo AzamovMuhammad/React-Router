2d9ba47c1cf1bf4d66408e60212ec68d63cc9c7f
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-08 23:12:18 +0500

    finished sidebar commonent

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,25 @@ import Sent from "./pages/Sent";
 import Drafts from "./pages/Drafts";
 import NotFoundPage from "./pages/NotFoundPage";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Inbox />}></Route>
-          <Route path="/starred" element={<Starred />}></Route>
-          <Route path="/snoozed" element={<Snoozed />}></Route>
-          <Route path="/sent" element={<Sent />}></Route>
-          <Route path="/drafts" element={<Drafts />}></Route>
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <div className="messagePart">
+        <BrowserRouter>
+        <Sidebar />
+          <Routes>
+            <Route path="/" element={<Inbox />}></Route>
+            <Route path="/starred" element={<Starred />}></Route>
+            <Route path="/snoozed" element={<Snoozed />}></Route>
+            <Route path="/sent" element={<Sent />}></Route>
+            <Route path="/drafts" element={<Drafts />}></Route>
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { ImPencil } from "react-icons/im";
+import { RiInboxFill } from "react-icons/ri";
+import { IoIosStarOutline } from "react-icons/io";
+import { MdAccessTime } from "react-icons/md";
+import { AiOutlineSend } from "react-icons/ai";
+import { IoDocumentOutline } from "react-icons/io5";
+
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+      <button className="compose"><ImPencil/> Compose</button>
+      <ul>
+        <li><Link to="/"><RiInboxFill className="icon"/> Inbox</Link></li>
+        <li><Link to="/"><IoIosStarOutline className="icon"/> Starred</Link></li>
+        <li><Link to="/"><MdAccessTime className="icon"/> Snoozed</Link></li>
+        <li><Link to="/"><AiOutlineSend className="icon"/> Sent</Link></li>
+        <li><Link to="/"><IoDocumentOutline className="icon"/> Drafts</Link></li>
+      </ul>
+    </div>
   )
 }
 
