c7f4a530c5ca41a898f0ecae6246c0dbee348999
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-10 12:44:11 +0500

    darfts added icon

--- src/pages/Drafts.jsx
+++ src/pages/Drafts.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineEmojiSad } from "react-icons/hi";
 
 function Drafts() {
   return (
-    <div className='draftPart'>
+    <div className="draftPart">
+      <i className="iconParts">
+        <HiOutlineEmojiSad />
+      </i>
       <h1>You don't have any saved drafts.</h1>
-      <h2>Saving a draft allows you to keep a message you aren't ready to send yet.</h2>
+      <h2>
+        Saving a draft allows you to keep a message you aren't ready to send
+        yet.
+      </h2>
     </div>
-  )
+  );
 }
 
-export default Drafts
+export default Drafts;
