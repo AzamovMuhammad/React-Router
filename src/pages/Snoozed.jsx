7fefd6c02fc8cb99ffb8515c3c47566a5550e63f
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-10 12:40:05 +0500

    snoozed finished

--- src/pages/Snoozed.jsx
+++ src/pages/Snoozed.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+
 function Snoozed() {
   return (
-    <div>Snoozed</div>
+    <div className='snoozedPart'>
+      <h1>No Snoozed messages! Do you want it!</h1>
+    </div>
   )
 }
 
