958aff311b00a5e13de40b29a7321d8b37ef77c9
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-10 12:47:08 +0500

    starred added icon

--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineEmojiSad } from "react-icons/hi";
 
 function Starred() {
   return (
-    <div className='starredPart'>
-      <h1>No starred messages. Stars let you give messages a special status to make them easier to find. To star a message, click on the star outline beside any message or conversation.</h1>
+    <div className="starredPart">
+      <i className="iconParts">
+        <HiOutlineEmojiSad />
+      </i>
+      <h1>
+        No starred messages. Stars let you give messages a special status to
+        make them easier to find. To star a message, click on the star outline
+        beside any message or conversation.
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Starred
+export default Starred;
