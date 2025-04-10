25c9b56bce50aceef6c8cb999630a33d14b435bc
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-10 12:43:29 +0500

    snooezed added icon

--- src/pages/Snoozed.jsx
+++ src/pages/Snoozed.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import { HiOutlineEmojiSad } from "react-icons/hi";
 
 function Snoozed() {
   return (
-    <div className='snoozedPart'>
+    <div className="snoozedPart">
+      <i className="iconParts">
+        <HiOutlineEmojiSad  />
+      </i>
       <h1>No Snoozed messages! Do you want it!</h1>
     </div>
-  )
+  );
 }
 
-export default Snoozed
+export default Snoozed;
