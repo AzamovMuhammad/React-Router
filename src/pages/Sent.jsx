4f6d6ef681e1a5a2f51c0b9e516bd27a65f81f91
AzamovMuhammad <azamov.dragon@gmail.com>
2025-04-10 12:44:54 +0500

    sent part added icon

--- src/pages/Sent.jsx
+++ src/pages/Sent.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { HiOutlineEmojiSad } from "react-icons/hi";
 
 function Sent() {
   return (
-    <div className='sentPart'>
+    <div className="sentPart">
+      <i className="iconParts">
+        <HiOutlineEmojiSad />
+      </i>
       <h1>No sent messages! Send one now!</h1>
     </div>
-  )
+  );
 }
 
-export default Sent
+export default Sent;
